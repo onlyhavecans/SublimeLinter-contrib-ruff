edb9d61e229af88036e0bba652636695dbfa5617
Amelia Aronsohn <squirrel@wearing.black>
2023-01-30 20:26:56 -0800

    Initial implementation of ruff: works for me™

--- linter.py
+++ linter.py
@@ -1,9 +1,12 @@
-from SublimeLinter.lint import Linter  # or NodeLinter, PythonLinter, ComposerLinter, RubyLinter
+from SublimeLinter.lint import PythonLinter
 
 
-class __class__(Linter):
-    cmd = '__cmd__'
-    regex = r''
+class ruff(PythonLinter):
+    cmd = ('ruff', '-')
+    regex = (
+        r'^(?P<filename>.+?):(?P<line>\d+):(?P<col>\d+): '
+        r'(?P<code>.+?) (?P<message>.+)$'
+    )
     multiline = False
     defaults = {
         'selector': 'source.python'
